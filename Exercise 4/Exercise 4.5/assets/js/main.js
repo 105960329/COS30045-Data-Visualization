46b00c04ee4a76ef9430aad7d5f901d520919d83
105960329 <105960329@student.swin.edu.au>
2026-09-17 13:17:49 +0800

    updated exercise4.5

--- Exercise 4/Exercise 4.5/assets/js/main.js
+++ Exercise 4/Exercise 4.5/assets/js/main.js
@@ -1,49 +1,44 @@
-
 const svg = d3.select(".responsive-svg-container")
-    .append("svg")
+  .append("svg")
     .attr("viewBox", "0 0 1200 1600")
     .style("border", "1px solid black");
 
-svg
-    .append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 414)
-    .attr("height", 16)
-    .attr("fill", "blue");
+ 
+d3.csv("assets/data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+  }).then(data => {
+  console.log(data);
+  console.log(data.length);
+  console.log(d3.max(data, d => d.count));
+  console.log(d3.min(data, d => d.count));
 
-d3.csv("assets/data/BrandCount.csv", d => {
-return {
-    brand: d.Brand_Reg,
-    count: +d["Count(SoldIn)"]
-};
+ 
+  data.sort((a, b) => b.count - a.count);
 
-}).then(data => {
-    
-    console.log(data);
-    console.log(data.length);
-    console.log(d3.max(data, d => d.count));
-    console.log(d3.min(data, d => d.count));
-    console.log(d3.extent(data, d => d.count));
-
-    drawBarChart(data);
-
+ 
+  drawBarChart(data);
+  
 });
 
 const drawBarChart = data => {
+  const barHeight = 20;
+  const barSpacing = 5;
 
-    const barHeight = 20;
-    const barSpacing = 5;
-
-    svg
-        .selectAll("rect")
-        .data(data)
-        .join("rect")
-        .attr("class", "bar")
-        .attr("x", 0)
-        .attr("y", (d, i) => i * (barHeight + barSpacing))
-        .attr("width", d => d.count)
-        .attr("height", barHeight)
-        .attr("fill", "steelblue");
-
+ 
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("class", d => {
+      console.log(d);
+      return `bar bar-${d.count}`;
+    })
+    .attr("width", d => d.count)
+    .attr("height", barHeight)
+    .attr("fill", "blue")
+     .attr("x", 0)
+    .attr("y", (d, i) => i * (barHeight + barSpacing));
 };
